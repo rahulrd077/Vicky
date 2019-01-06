523da9e2d7111189ad45fb0eec4cf4f3ebc14e03
Rahul Das <rahulrd077@gmail.com>
2019-01-06 13:51:39 +0530

    Ting Tong its a first commit.

---
+++ src/main/java/testbase/MavenProject/Testbase.java
@@ -0,0 +1,48 @@
+package testbase.MavenProject;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
+public class Testbase {
+	
+	 public static WebDriver driver ;
+	 public static  Properties prop;
+	
+	public Testbase() {
+		
+		
+		try {
+			
+			prop = new Properties();			
+			
+		FileInputStream file = new FileInputStream("D:\\Eclipse\\Workspace\\MavenProject\\Objectrepository.properties");
+		
+		
+		prop.load(file);
+		 
+	}
+	
+		catch(Exception e) {
+			
+			e.printStackTrace();
+	
+
+}
+	}
+		
+		public static void initialization() {
+			
+			String browserName = prop.getProperty("");
+			
+			if (browserName.equals("chrome"))
+					{
+				
+				
+}
+			
+		}
+		
+
+}
